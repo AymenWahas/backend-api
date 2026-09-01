63ddad9b419e103a81f8fe2a2335ce072d345efd
Ayman Wahas <aymenwahas@gmail.com>
2026-09-01 19:07:04 +0300

    initial backend api setup

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/health", healthHandler)
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8443"
+	}
+
+	addr := ":" + port
+
+	fmt.Println("HTTPS server running on", addr)
+
+	err := http.ListenAndServeTLS(
+		addr,
+		"certs/cert.pem",
+		"certs/key.pem",
+		nil,
+	)
+
+	if err != nil {
+		fmt.Println("Server error:", err)
+	}
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, `{"status":"ok"}`)
+}
